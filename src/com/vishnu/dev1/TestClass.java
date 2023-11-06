e22cbcab85ac9bb4f446a8af65a704d71747cac3
Vishnu CP <vishnu.7210@gmail.com>
2023-11-06 17:18:23 +0530

    commit dev1

--- src/com/vishnu/dev1/TestClass.java
+++ src/com/vishnu/dev1/TestClass.java
@@ -4,7 +4,13 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
-
+	static void print(){
+		
+		System.out.println("Print Dev1");
+	}
+	
 }
